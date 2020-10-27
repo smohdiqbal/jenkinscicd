5848be0973a0d31582c9ee7b76f9b46a57444ec4
mohammed iqbal <73151311+smohdiqbal@users.noreply.github.com>
2020-10-27 23:24:29 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from github" );
+        System.out.println( "Hello from mvn project" );
     }
 }
